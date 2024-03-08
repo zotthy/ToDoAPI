25161b28a1700b9e362b34ef243d03d0b8b71720
zotthy <se05110800@gmail.com>
2024-03-08 20:45:15 +0100

    mistake

--- src/main/java/projekt/beta/Config/SecurityConfiguration.java
+++ src/main/java/projekt/beta/Config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,9 +19,8 @@ import projekt.beta.Security.BearerTokenFilter;
 import projekt.beta.Security.JwtAuthenticationFilter;
 import projekt.beta.Security.JwtService;
 
-
+@Profile("fac")
 @Configuration
-@Profile("full")
 public class SecurityConfiguration {
     private final JwtService jwtService;
 
@@ -40,7 +40,12 @@ public class SecurityConfiguration {
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtService);
         BearerTokenFilter bearerTokenFilter = new BearerTokenFilter(jwtService);
         http.authorizeHttpRequests(requests -> requests
-                .anyRequest().permitAll());
+                .requestMatchers(mvc.pattern("/auth/register")).permitAll()
+                .requestMatchers(mvc.pattern("/auth/login")).permitAll()
+                .requestMatchers(mvc.pattern(HttpMethod.POST,"/addTask")).hasRole("USER")
+                .requestMatchers(mvc.pattern(HttpMethod.GET,"/tasks")).hasRole("USER")
+                .requestMatchers(mvc.pattern(HttpMethod.GET,"/tasks/{id}")).hasRole("USER")
+                .requestMatchers(mvc.pattern(HttpMethod.GET,"/profile")).hasRole("USER"));
         http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(csrfCustomizer -> csrfCustomizer.disable());
         http.addFilterBefore(jwtAuthenticationFilter, AuthorizationFilter.class);
@@ -53,5 +58,3 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 }
-
-
