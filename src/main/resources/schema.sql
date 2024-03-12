30f08df1537d63498334d3f1da5580412fdebafb
zotthy <se05110800@gmail.com>
2024-03-12 17:39:52 +0100

    final

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Category
+CREATE TABLE category
 (
     id   BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Task
+CREATE TABLE task
 (
     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
     title       VARCHAR(255) NOT NULL,
@@ -13,10 +13,10 @@ CREATE TABLE Task
     priority    VARCHAR(50),
     status      VARCHAR(50),
     category_id BIGINT,
-    FOREIGN KEY (category_id) REFERENCES Category (id)
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
-CREATE TABLE Address
+CREATE TABLE address
 (
     id       BIGINT PRIMARY KEY AUTO_INCREMENT,
     street   VARCHAR(30),
@@ -38,7 +38,7 @@ CREATE TABLE members
     email      VARCHAR(30) UNIQUE NOT NULL,
     password   VARCHAR(200)        NOT NULL,
     address_id BIGINT,
-    FOREIGN KEY (address_id) REFERENCES Address (id)
+    FOREIGN KEY (address_id) REFERENCES address (id)
 );
 
 CREATE TABLE user_roles
@@ -50,11 +50,11 @@ CREATE TABLE user_roles
     FOREIGN KEY (role_id) REFERENCES role (id)
 );
 
-CREATE TABLE Assignment
+CREATE TABLE assignment
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
     task_id    BIGINT,
     members_id BIGINT,
-    FOREIGN KEY (task_id) REFERENCES Task (id),
+    FOREIGN KEY (task_id) REFERENCES task (id),
     FOREIGN KEY (members_id) REFERENCES members (id)
 );
